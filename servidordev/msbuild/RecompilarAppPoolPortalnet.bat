e3f88204ec648e1e713af13d50db59df40e8d9c4
Carlos Melo <carlaofernandesedu@gmail.com>
2016-05-11 15:19:26 -0300

    Atualizando os fontes

---
+++ servidordev/msbuild/RecompilarAppPoolPortalnet.bat
@@ -0,0 +1,5 @@
+@Echo off
+@Echo. PROCESSSO DE COMPILACAO INICIADO
+@Echo. Compilando a aplicacao 
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\aspnet_compiler.exe  -v /portalnetpreproducao
+Echo. PROCESSSO DE COMPILACAO FINALIZADO
